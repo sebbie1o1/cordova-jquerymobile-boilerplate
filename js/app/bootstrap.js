391dee9ca9f1687d123caa527ae8cce4d548a9be
sebbie <sebbie@sebbie.pl>
2012-04-29 11:49:17 +0200

    src files

---
+++ js/app/bootstrap.js
@@ -0,0 +1,27 @@
+(function() {
+  var device_ready = false;
+  var jqm_mobile_init = false;
+
+  var initApp = function() {
+    if (device_ready && jqm_mobile_init) {
+      startApp();
+    }
+  };
+
+  var onDeviceReady = function() {
+    device_ready = true;
+    //alert('dev ready');
+    initApp();
+  };
+
+  var onMobileInit = function() {
+    $.support.cors = true;
+    $.mobile.allowCrossDomainPages = true;
+    jqm_mobile_init = true;
+    //alert('jqm ready');
+    initApp();
+  };
+
+  $(document).bind('mobileinit', onMobileInit);
+  document.addEventListener("deviceready", onDeviceReady, false);
+})();
